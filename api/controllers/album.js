012052fae7e474f59e86cf257f1ea292a4a5f603
sufikaur <sufikaur@gmail.com>
2020-07-20 13:05:04 -0700

    add basic search (issues in comments)

--- api/controllers/album.js
+++ api/controllers/album.js
@@ -1,10 +1,67 @@
 // function: allAlbums
 // sends all parent albums
-export async function allAlbums(req, res) {
+
+import { query } from "express";
+
+/**
+ * /albums : all albums
+ * /albums?nameincl={name} : Search for album that matches this name (fuzzy match)
+ * /albums?tag={tag} : Search for albums that have these tags (fuzzy matches)
+ * /albums?keyword={keyword} : Search for albums that have these keywords (fuzzy matches)
+ * /albums?parentid={parentid} : Search for albums that has this albumId as a parent.
+ * /albums?updatedafter={date} : Search for albums that were last updated after this date.
+ */
+export async function albumsBy(req,res) {
   let conn;
+  const nameIncludes = req.query.nameincl;
+  const tag = req.query.tag;
+  const parentID = parseInt(req.query.parentid, 10);
+  const date = req.query.date;
+  const keyword = req.query.keyword;
+  const queryRes = getAlbumQuery(nameIncludes, tag, parentID, date, keyword, null);
+  const q = queryRes[0];
+  const params = queryRes[1];
+
+  // temp for testing
+  console.log(nameIncludes);
+  console.log(tag);
+  console.log(parentID);
+  console.log(date);
+  console.log(keyword);
+  console.log(q);
+  console.log(params);
+  try{
+    conn = await req.app.locals.pool.getConnection();
+    //console.log(await conn.query(q, params));
+    const result = await conn.query(q)
+    //console.log(result);
+    res.json(result);
+  } 
+  catch (err) {
+    res.json(`error${err}`);
+    return;
+  }
+  finally {
+    if (conn) {
+      conn.end();
+    }
+  }
+};
+
+// function:byAlbumID
+// sends the album with the given id
+export async function byAlbumID(req, res) {
+  let conn;
+  const albumID = parseInt(req.params.albumID, 10);
+  const queryRes = getAlbumQuery(null, null, null, null, null, albumID);
+  const q = queryRes[0];
+  const params = queryRes[1];
+
+  // temp for testing
+  console.log(q);
+  console.log(albumID);
   try {
     conn = await req.app.locals.pool.getConnection();
-    const q = 'SELECT * FROM Album WHERE Parent IS NULL;';
     const result = await conn.query(q);
     res.json(result);
     return;
@@ -17,24 +74,65 @@ export async function allAlbums(req, res) {
     }
   }
 }
+/**
+ * function: getAlbumQuery
+ * creates query and parameters for an album query to the database
+ * currently does not work with prepared statements
+ * another issue is that searches are case sensitive
+ *  when does a parent ID mater and when does it not?
+ * 
+ * @param {String} name - string for fuzzy search
+ * @param {String} tag - tag for fuzzy search
+ * @param {Int} parentID - parentID for exact search
+ * @param {String} date - updated after given date
+ * @param {String} keyword - keyword for fuzzy search
+ * @param {Int} albumID - albumID for exact search
+ * 
+ * @return {Object[String, Array]} = template statement, parameters for template statement
+ */
+function getAlbumQuery(name, tag, parentID, date, keyword, albumID) {
+  const cols = `ID, Title, Parent, Tags, Keywords, Artists, Updated`;
+  var params = [];
+  var q = ""; 
+  const basicQuery = `SELECT ${cols} FROM Album`; 
+  //parent
+  if(parentID) {
+    q = q + ` WHERE Album.Parent = ${parentID}`;
+    params.push(parentID);
 
-// // function:byAlbumID
-// // sends the album with the given id
-export async function byAlbumID(req, res) {
-  let conn;
-  const albumID = parseInt(req.params.albumID, 10);
-  try {
-    conn = await req.app.locals.pool.getConnection();
-    const q = 'SELECT * FROM `Album` WHERE `ID` = ?;';
-    const result = await conn.query(q, albumID);
-    res.json(result);
-    return;
-  } catch (err) {
-    res.json(`error${err}`);
-    return;
-  } finally {
-    if (conn) {
-      conn.end();
-    }
+  } else if (!albumID){
+    q = q + ` WHERE Album.Parent IS NULL`;
   }
+  //name
+  if(name) {
+    q = q + ` AND Album.Title LIKE '%${name}%'`;
+    params.push(name);
+  }
+  //tag
+  if(tag) {
+    q = q + ` AND Album.Tags LIKE '%${tag}%'`;
+    params.push(tag);
+  }
+  //keyword
+  if(tag) {
+    q = q + ` AND Album.Keywords LIKE '%${keyword}%'`;
+    params.push(keyword);
+  }
+  //date
+  if(date) {
+    q = q + ` AND Album.Updated > '${date}'`;
+    params.push(date);
+  }
+  //keyword
+  if(keyword) {
+    q = q + ` AND Album.Keywords LIKE '%${keyword}%'`;
+    params.push(date);
+  }
+  //album id
+  if(albumID) {
+    q = q + ` WHERE Album.ID = ${albumID}`;
+    params.push(albumID);
+  }
+  // return an object with a query and its parameters
+  return [basicQuery + q + `;`, params];
 }
--- api/controllers/track.js
+++ api/controllers/track.js
@@ -1,22 +1,77 @@
-// function: multipleTracks
-// if no specified album id, sends all tracks in the database
-// if specified album id, returns all tracks in the given album
-export async function multipleTracks(req, res) {
+import { query } from "express";
+
+/**
+ * /tracks : all tracks
+ * /tracks?albumid={albumID} : Search for tracks that have this albumID
+ * /tracks?nameincl={nameIncludes} : Search for tracks that matches this name (fuzzy match)
+ * /tracks?lengthgreater={length} : Search for tracks are longer than X seconds.
+ * /tracks?type={type} : Search for tracks that match this type (exact match)
+ * /tracks?artistid={artistid} : Search for tracks that have this artistId
+ */
+export async function tracksBy(req,res) {
   let conn;
-  let q;
-  const albumid = parseInt(req.query.albumid, 10);
-  let result;
+  const albumID = parseInt(req.query.albumid, 10);
+  const artistID = parseInt(req.query.artistid, 10);
+  const nameIncludes = req.query.nameincl;
+  const length = req.query.length;
+  const type = req.query.type;
+
+  console.log(albumID);
+  console.log(artistID);
+  console.log(nameIncludes);
+  console.log(length);
+  console.log(type);
+  
+  const queryRes = getTrackQuery(albumID, artistID, nameIncludes, length, type, null);
+  const q = queryRes[0];
+  const params = queryRes[1];
+
+  // temp for testing
+  console.log(albumID);
+  console.log(artistID);
+  console.log(nameIncludes);
+  console.log(length);
+  console.log(type);
+
+  console.log(q);
+  console.log(params);
+  try{
+    conn = await req.app.locals.pool.getConnection();
+    //console.log(await conn.query(q, params));
+    const result = await conn.query(q)
+    //console.log(result);
+    res.json(result);
+  } 
+  catch (err) {
+    res.json(`error${err}`);
+    return;
+  }
+  finally {
+    if (conn) {
+      conn.end();
+    }
+  }
+  
+};
+
+/**
+ * function: byTrackID
+ * sends the track with the given id
+ * @param {Request} req 
+ * @param {Response} res 
+ */
+export async function byTrackID(req, res) {
+  let conn;
+  const trackID = parseInt(req.params.trackID, 10);
+  const queryRes = getTrackQuery(null, null, null, null, null, trackID);
+  const q = queryRes[0];
+  const params = queryRes[1];
+  console.log(q);
+  console.log(params);
+
   try {
     conn = await req.app.locals.pool.getConnection();
-    if (albumid) {
-      q =
-        'SELECT * FROM `TrackAlbum` LEFT JOIN `Track`' +
-        'ON `TrackAlbum`.`Album` = `Track`.`ID` WHERE `Album` = ?';
-      result = await conn.query(q, [albumid]);
-    } else {
-      q = 'SELECT * FROM `Track`;';
-      result = await conn.query(q);
-    }
+    const result = await conn.query(q);
     res.json(result);
     return;
   } catch (err) {
@@ -28,24 +83,57 @@ export async function multipleTracks(req, res) {
     }
   }
 }
+/**
+ * function: getAlbumQuery
+ * creates query and parameters for an album query to the database
+ * currently does not work with prepared statements
+ * 
+ * @param {String} albumID - string for fuzzy search
+ * @param {String} artistID - tag for fuzzy search
+ * @param {Int} name - parentID for exact search
+ * @param {String} length - updated after given date
+ * @param {String} type - keyword for fuzzy search
+ * @param {Int} trackID - albumID for exact search
+ * 
+ * @return {Object[String, Array]} = template statement, parameters for template statement
+ */
+function getTrackQuery(albumID, artistID, name, length, type, trackID) {
+  const cols = `ID, Title, Media, Length, Updated`;
+  var params = [];
+  var q = ""; 
+  const basicQuery = `SELECT * FROM Track LEFT JOIN TrackAlbum ON TrackAlbum.Album = Track.ID WHERE 1=1`; 
+  //album id
+  if(albumID) {
+    q = q + ` AND Album = ${albumID}`;
+    params.push(albumID);
 
-// function: byTrackID
-// sends the track with the given id
-export async function byTrackID(req, res) {
-  let conn;
-  const trackID = parseInt(req.params.trackID, 10);
-  try {
-    conn = await req.app.locals.pool.getConnection();
-    const q = 'SELECT * FROM `Track` WHERE `Track`.`ID` = ?;';
-    const result = await conn.query(q, [trackID]);
-    res.json(result);
-    return;
-  } catch (err) {
-    res.json(`error${err}`);
-    return;
-  } finally {
-    if (conn) {
-      conn.end();
-    }
   }
+  //artist id
+  // how does one search within a json arr
+  if(artistID) {
+    q = q + ` AND Artist = ${artistID}`;
+    params.push(artistID);
+  }
+  //name
+  if(name) {
+    q = q + ` AND Title LIKE '%${name}%'`;
+    params.push(name);
+  }
+  //length
+  if(length) {
+    q = q + ` AND Length > '${length}'`;
+    params.push(length);
+  }
+  //type
+  if(type) {
+    q = q + ` AND Type = '${type}'`;
+    params.push(type);
+  }
+  //trackID
+  if(trackID) {
+    q = q + ` AND ID = ${trackID}`;
+    params.push(trackID);
+  }
+  // return an object with a query and its parameters
+  return [basicQuery + q + `;`, params];
 }
