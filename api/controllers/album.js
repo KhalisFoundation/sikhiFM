a3b3e9f8197ab4e2fd5ea28d364fdf57ac9b6d27
sufikaur <sufikaur@gmail.com>
2020-07-22 14:13:13 -0700

    advance search incomplete

--- api/controllers/album.js
+++ api/controllers/album.js
@@ -2,26 +2,36 @@
 // sends all parent albums
 
 /**
- * /albums : all albums
+ * function: all Albums
+ * sends all albums according to the given to the search query
+ * if none are specified, all parent albums are returned
+ * 
+ * @param {Request} req - Express request object
+ * @param {Response} res - Express response object
+ * 
+ * /albums : all parent albums
  * /albums?name={name} : Search for album that matches this name (fuzzy match)
  * /albums?tag={tag} : Search for albums that have these tags (fuzzy matches)
  * /albums?keyword={keyword} : Search for albums that have these keywords (fuzzy matches)
  * /albums?parentID={parentID} : Search for albums that has this albumId as a parent.
  * /albums?updated={updated} : Search for albums that were last updated after this date.
+ * /albums?artistName={artistName} : Search for all albums that have this artist (exact match) 
+ * /albums?artistID={artistID} : Search for all albums that have this artist (exact match)
+ * /albums?lyric={lyric} : Search for all albums that contain a track with these lyrics (fuzzy match)
  */
 export async function albumsBy(req, res) {
   let conn;
-  const { name } = req.query;
-  const { tag } = req.query;
-  const { keyword } = req.query;
+  const { name, tag, keyword, updated, artistName, artistID, lyric } = req.query;
   const parentID = parseInt(req.query.parentID, 10);
-  const { updated } = req.query;
   const { query, params } = getAlbumQuery({
     name,
     tag,
     parentID,
     updated,
     keyword,
+    artistName,
+    artistID,
+    lyric
   });
   try {
     conn = await req.app.locals.pool.getConnection();
@@ -39,6 +49,11 @@ export async function albumsBy(req, res) {
 
 // function:byAlbumID
 // sends the album with the given id
+/**
+ * function: byAlbumID
+ * @param {Request} req - Express request object
+ * @param {Response} res - Express response object
+ */
 export async function byAlbumID(req, res) {
   let conn;
   const albumID = parseInt(req.params.albumID, 10);
@@ -67,14 +82,28 @@ export async function byAlbumID(req, res) {
  * @param {String} date - updated after given date
  * @param {String} keyword - keyword for fuzzy search
  * @param {Int} albumID - albumID for exact search
- *
+ * @param {Int} artistName - artistName for fuzzy search
+ * @param {Int} artistID - artistID for exact search
+ * @param {Int} lyric - 
+ * 
  * @return {Object[String, Array]} = template statement, parameters for template statement
  */
-function getAlbumQuery({ name, tag, parentID, updated, keyword, albumID }) {
-  const cols ='ID, Title, Parent, Tags, Keywords, Artists, Updated';
-  const params = [];
+function getAlbumQuery({ name, tag, parentID, updated, keyword, artistName, albumID, artistID, lyric }) {
+  var cols = 'Album.ID, Album.Title, Album.Parent, Album.Tags, Album.Keywords, Album.Artists, Album.Updated';
+  var params = [];
   let q = '';
-  const basicQuery = `SELECT ${cols} FROM Album WHERE 1=1`;
+  var basicQuery = `SELECT ${cols} FROM Album WHERE 1=1`;
+    // artist's id
+    if(artistID) {
+      cols = `Album.ID, Album.Title, Album.Parent, Album.Tags, ` +
+      `Album.Keywords, Album.Artists, Album.Updated, Artist.ID as ArtistID, ` +
+      `Artist.Name, Artist.NameGurmukhi, Artist.Description, Artist.Detail, ` +
+      `Artist.Tags as ArtistTags, Artist.Keywords as ArtistKeywords `;
+      basicQuery = `SELECT ${cols} FROM Artist ` +
+      `INNER JOIN Album ON JSON_CONTAINS(Album.Artists, ?) = Artist.ID ` +
+      `WHERE 1=1`; 
+      params.push(artistID.toString())
+    }
   // parent
   if (parentID) {
     q += ' AND Album.Parent = ?';
@@ -105,5 +134,10 @@ function getAlbumQuery({ name, tag, parentID, updated, keyword, albumID }) {
     q += ' AND Album.ID = ?';
     params.push(albumID.toString());
   }
+
+  // lyric
+  // if(lyric) {
+
+  // }
   return { query: `${basicQuery} ${q};`, params };
 }
--- api/controllers/track.js
+++ api/controllers/track.js
@@ -5,21 +5,28 @@
  * /tracks?length={length} : Search for tracks are longer than X seconds.
  * /tracks?type={type} : Search for tracks that match this type (exact match)
  * /tracks?artistID={artistID} : Search for tracks that have this artistId
+ * /tracks?artistName={artistName} : Search for tracks by artist name (fuzzy match)
+ * /tracks?location={location} : Search for tracks by country name/city name/state name (fuzzy match)
+ * /tracks?lyric={lyric} : Search for tracks that have these lyrics as the main line (fuzzy match)
  */
 export async function tracksBy(req, res) {
   let conn;
   const albumID = parseInt(req.query.albumID, 10);
   const artistID = parseInt(req.query.artistid, 10);
-  const { name } = req.query;
-  const { length } = req.query;
-  const { type } = req.query;
+  const { name, length, type, artistName, location, lyric } = req.query;
   const { query, params } = getTrackQuery({
     albumID,
     artistID,
     name,
     length,
     type,
+    artistName,
+    location, 
+    lyric
   });
+
+  console.log(location);
+  console.log(query, params);
 
   try {
     conn = await req.app.locals.pool.getConnection();
@@ -78,18 +85,33 @@ export async function byTrackID(req, res) {
  * @return {Object[String, Array]} = template statement, parameters for template statement
  */
 
-function getTrackQuery({
-  albumID,
-  artistID,
-  name,
-  length,
-  type,
-  trackID,
-}) {
-  const cols = `ID, Title, Media, Length, Updated`;
-  const params = [];
-  let q = '';
-  const basicQuery = `SELECT ${cols} FROM Track LEFT JOIN TrackAlbum ON TrackAlbum.Album = Track.ID WHERE 1=1`;
+function getTrackQuery({ albumID, artistID, name, length, type, trackID, artistName, location, lyric }) {
+  var cols = `Track.ID, Track.Title, Track.Media, TrackAlbum.Track, TrackAlbum.Album, Track.Length, Track.Updated`;
+  var params = [];
+  var q = '';
+  var basicQuery = `FROM Track LEFT JOIN TrackAlbum ON TrackAlbum.Album = Track.ID WHERE 1=1`;
+  //artist's name
+  if(artistName) {
+    cols +=  `Artist.ID as ArtistID, Artist.Name, Artist.NameGurmukhi, Artist.Description, `+
+    `Artist.Detail, Artist.Tags as ArtistTags, Artist.Keywords as ArtistKeywords `;
+    basicQuery =  `FROM Track `+
+    `LEFT JOIN Artist ON Track.Artist = Artist.ID `+
+    `WHERE Artist.Name Like ?`
+    params.push(`%${artistName}%`)
+  }
+  //location
+  if(location) {
+    basicQuery = `SELECT Track.ID, Track.Title, Track.Media, Track.Length, Track.Updated, ` +
+    `Location.City, Location.State, Location.Country, Location.Nickname as LocationNickname ` +
+    `FROM Track ` + 
+    `LEFT JOIN Location ON Track.Location = Location.ID ` +
+    `WHERE City Like ? ` +
+    `OR State LIKE ? ` +
+    `OR Country LIKE ?`;
+    params.push(`%${location}%`);
+    params.push(`%${location}%`);
+    params.push(`%${location}%`);
+  }
   // album id
   if (albumID) {
     q += ' AND Album = ?';
@@ -120,5 +142,11 @@ function getTrackQuery({
     q += ' AND ID = ?';
     params.push(trackID.toString());
   }
-  return { query: `${basicQuery + q};`, params };
+  
+  //lyric
+  if(lyric) {
+    //basicQuery =
+    params.push(lyric)
+  }
+  return { query: `SELECT ${cols + basicQuery + q};`, params };
 }
